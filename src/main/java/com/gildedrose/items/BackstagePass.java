9c101564b03b8f0091ae708e94f904b34525aa94
Linesh <linesh.simpcity@gmail.com>
2020-01-14 21:41:33 +0800

    refactor: replace other references to increaseQuality()

--- src/main/java/com/gildedrose/items/BackstagePass.java
+++ src/main/java/com/gildedrose/items/BackstagePass.java
@@ -14,19 +14,14 @@ public class BackstagePass extends Item {
 
     @Override
     protected void updateQuality() {
-        if (quality < 50) {
-            quality = quality + 1;
+        increaseQuality();
 
-            if (sellIn < 11) {
-                increaseQuality();
-            }
+        if (sellIn < 11) {
+            increaseQuality();
+        }
 
-            if (sellIn < 6) {
-                if (quality < 50) {
-                    quality = quality + 1;
-                }
-            }
-
+        if (sellIn < 6) {
+            increaseQuality();
         }
     }
 
