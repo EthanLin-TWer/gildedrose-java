26a401f553eb9faed4297c39a10e3660fbd5e20e
Linesh <linesh.simpcity@gmail.com>
2020-01-14 13:35:36 +0800

    refactor: extract sellIn and quality before extracting createAgedBrie()

--- src/test/java/com/gildedrose/TextTestFixture.java
+++ src/test/java/com/gildedrose/TextTestFixture.java
@@ -14,9 +14,11 @@ public class TextTestFixture {
 
         printStream.println("OMGHAI!");
 
+        final int sellIn = 2;
+        final int quality = 0;
         Item[] items = new Item[] {
                 new Item("+5 Dexterity Vest", 10, 20), //
-                new Item("Aged Brie", 2, 0), //
+                new Item("Aged Brie", sellIn, quality), //
                 new Item("Elixir of the Mongoose", 5, 7), //
                 new Item("Sulfuras, Hand of Ragnaros", 0, 80), //
                 new Item("Sulfuras, Hand of Ragnaros", -1, 80),
