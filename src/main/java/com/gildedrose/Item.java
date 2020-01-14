405c983eddca0e8225bc740ecdd5064533661476
Linesh <linesh.simpcity@gmail.com>
2020-01-14 21:25:30 +0800

    refactor: revert if condition - handling isSulfuras() - continue reverting

--- src/main/java/com/gildedrose/Item.java
+++ src/main/java/com/gildedrose/Item.java
@@ -34,14 +34,15 @@ public class Item {
             return;
         }
 
-        if (!isBackstagePass()) {
-            if (quality > 0) {
-                if (!isSulfuras()) {
-                    quality = quality - 1;
-                }
-            }
-        } else {
+        if (isBackstagePass()) {
             quality = 0;
+            return;
+        }
+
+        if (quality > 0) {
+            if (!isSulfuras()) {
+                quality = quality - 1;
+            }
         }
 
     }
