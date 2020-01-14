67afd04fa59b434a5b1c86660a3ad52a102ee5f3
Linesh <linesh.simpcity@gmail.com>
2020-01-14 21:28:53 +0800

    refactor: revert if condition

--- src/main/java/com/gildedrose/Item.java
+++ src/main/java/com/gildedrose/Item.java
@@ -39,10 +39,9 @@ public class Item {
             return;
         }
 
-        if (quality <= 0) {
-            return;
+        if (quality > 0) {
+            quality = quality - 1;
         }
-        quality = quality - 1;
     }
 
     private boolean isExpired() {
