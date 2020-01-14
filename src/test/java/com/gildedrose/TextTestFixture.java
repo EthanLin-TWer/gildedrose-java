e238fb3882d8d74598373f7708519941d2f07212
Linesh <linesh.simpcity@gmail.com>
2020-01-14 13:38:28 +0800

    refactor: inline sell in and quality back

--- src/test/java/com/gildedrose/TextTestFixture.java
+++ src/test/java/com/gildedrose/TextTestFixture.java
@@ -14,11 +14,9 @@ public class TextTestFixture {
 
         printStream.println("OMGHAI!");
 
-        final int sellIn = 2;
-        final int quality = 0;
         Item[] items = new Item[] {
                 new Item("+5 Dexterity Vest", 10, 20), //
-                Item.createAgedBrie(sellIn, quality),
+                Item.createAgedBrie(2, 0),
                 new Item("Elixir of the Mongoose", 5, 7), //
                 new Item("Sulfuras, Hand of Ragnaros", 0, 80), //
                 new Item("Sulfuras, Hand of Ragnaros", -1, 80),
