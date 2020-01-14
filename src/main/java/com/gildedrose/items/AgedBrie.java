ce8969f08c3a85ce09f553c7bdbe645fbe6b5929
Linesh <linesh.simpcity@gmail.com>
2020-01-14 20:55:43 +0800

    refactor: create AgedBrie subclass

---
+++ src/main/java/com/gildedrose/items/AgedBrie.java
@@ -0,0 +1,9 @@
+package com.gildedrose.items;
+
+import com.gildedrose.Item;
+
+public class AgedBrie extends Item {
+    public AgedBrie(String name, int sellIn, int quality) {
+        super(name, sellIn, quality);
+    }
+}
