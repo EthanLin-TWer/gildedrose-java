d6e6e1253dd373c904cd9f70a62fe7244ee5e1ef
Linesh <linesh.simpcity@gmail.com>
2020-01-14 12:40:20 +0800

    initial version of gilded rose

---
+++ src/test/java/com/gildedrose/GildedRoseTest.java
@@ -0,0 +1,20 @@
+package com.gildedrose;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class GildedRoseTest {
+
+    @Test
+    public void foo() {
+        Item[] items = new Item[] { new Item("foo", 1, 5) };
+        GildedRose app = new GildedRose(items);
+        app.update_quality();
+        assertEquals("foo", app.items[0].name);
+        assertThat(app.items[0].quality, is(4));
+        assertThat(app.items[0].sell_in, is(0));
+    }
+
+}
