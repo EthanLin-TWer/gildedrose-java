6635e24b79d2908662719b12618815ba2e0f3887
Linesh <linesh.simpcity@gmail.com>
2020-01-14 21:37:40 +0800

    refactor: remove backstage pass logic from isBackstagePass class

--- src/main/java/com/gildedrose/items/BackstagePass.java
+++ src/main/java/com/gildedrose/items/BackstagePass.java
@@ -19,25 +19,21 @@ public class BackstagePass extends Item {
 
     @Override
     protected void updateQuality() {
-        if (isBackstagePass()) {
-            if (quality < 50) {
-                quality = quality + 1;
+        if (quality < 50) {
+            quality = quality + 1;
 
-                if (isBackstagePass()) {
-                    if (sellIn < 11) {
-                        if (quality < 50) {
-                            quality = quality + 1;
-                        }
-                    }
-
-                    if (sellIn < 6) {
-                        if (quality < 50) {
-                            quality = quality + 1;
-                        }
-                    }
+            if (sellIn < 11) {
+                if (quality < 50) {
+                    quality = quality + 1;
                 }
             }
-            return;
+
+            if (sellIn < 6) {
+                if (quality < 50) {
+                    quality = quality + 1;
+                }
+            }
+
         }
     }
 }
