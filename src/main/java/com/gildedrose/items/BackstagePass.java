e1a7b370041a1d6a7acd9caed5c528e7cb49448e
Linesh <linesh.simpcity@gmail.com>
2020-01-14 21:00:42 +0800

    refactor: create BackstagePass subclass

---
+++ src/main/java/com/gildedrose/items/BackstagePass.java
@@ -0,0 +1,9 @@
+package com.gildedrose.items;
+
+import com.gildedrose.Item;
+
+public class BackstagePass extends Item {
+    public BackstagePass(int sellIn, int quality) {
+        super("Sulfuras, Hand of Ragnaros", sellIn, quality);
+    }
+}
