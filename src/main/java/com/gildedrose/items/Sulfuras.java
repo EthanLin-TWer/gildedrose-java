33336540fd6872a220abe38529cc77dff41057f3
来一 <tom.lai19871106@gmail.com>
2020-02-01 18:11:12 +0800

    重构：创建Sulfuras子类

---
+++ src/main/java/com/gildedrose/items/Sulfuras.java
@@ -0,0 +1,13 @@
+package com.gildedrose.items;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: lai.yi
+ * Date: 2020/2/1
+ * Description:
+ **/
+public class Sulfuras extends Item {
+    public Sulfuras(int sellIn, int quality) {
+        super("Sulfuras, Hand of Ragnaros", sellIn, quality);
+    }
+}
