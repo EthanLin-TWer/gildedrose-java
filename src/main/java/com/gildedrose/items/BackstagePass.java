3ca81d4f9c178c94529467960da8f3eb43791b72
来一 <tom.lai19871106@gmail.com>
2020-02-01 18:09:54 +0800

    重构：创建BackstagePass子类

---
+++ src/main/java/com/gildedrose/items/BackstagePass.java
@@ -0,0 +1,13 @@
+package com.gildedrose.items;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: lai.yi
+ * Date: 2020/2/1
+ * Description:
+ **/
+public class BackstagePass extends Item {
+    public BackstagePass(int sellIn, int quality) {
+        super("Backstage passes to a TAFKAL80ETC concert", sellIn, quality);
+    }
+}
