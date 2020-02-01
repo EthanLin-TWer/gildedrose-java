c586f94969783fe463ffe919a19e51c339c2d3d3
来一 <tom.lai19871106@gmail.com>
2020-02-01 18:08:53 +0800

    重构：创建AgedBrie子类

---
+++ src/main/java/com/gildedrose/items/AgedBrie.java
@@ -0,0 +1,13 @@
+package com.gildedrose.items;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: lai.yi
+ * Date: 2020/2/1
+ * Description:
+ **/
+public class AgedBrie extends Item {
+    public AgedBrie(int sellIn, int quality) {
+        super("Aged Brie", sellIn, quality);
+    }
+}
